0fa90a76d8fb122b1a2348b3d0815b14f7494b8c
rafaelapessoa <rafaela_tunu@hotmail.com>
2024-06-05 11:20:48 +0200

    new object

--- src/index.js
+++ src/index.js
@@ -11,3 +11,50 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+server.get('/movies', (req, res) => {
+  const fakeMovies = [
+    {
+      id: 1,
+      title: "Wonder Woman",
+      genre: "Action",
+      image:
+        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
+      category: "Superhero",
+      year: 2017,
+      director: "Patty Jenkins",
+    },
+    {
+      id: 2,
+      title: "Inception",
+      genre: "Science Fiction",
+      image:
+        "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
+      category: "Thriller",
+      year: 2010,
+      director: "Christopher Nolan",
+    },
+  ];
+  res.json({success: true,movies:  fakeMovies});
+});
+
+server.post('/public/movies/images', (req, res) => {
+  const fakeMovies = [
+    {
+      id: 3,
+    title: "Gambita de dama",
+    genre: "Drama",
+    image:
+        "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg"
+},
+  {
+    id: 4,
+    title: "Friends",
+    genre: "Comedia",
+    image:
+        "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg"
+  }
+  ]
+  res.json({success: true,movies:  fakeMovies});
+});
+
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -3,7 +3,7 @@
 const getMoviesFromApi = () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//localhost:4000')
+  return fetch('//localhost:4000/movies')
     .then(response => response.json())
     .then(data => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
@@ -11,23 +11,7 @@ const getMoviesFromApi = () => {
     });
 };
 
-server.get('/movies', (req, res) => {
-  const data = [
-    {
-      id: '1',
-      title: 'Gambita de dama',
-      genre: 'Drama',
-      image:
-      '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-  },{   
-      id: '2',
-      title: 'Friends',
-      genre: 'Comedia',
-      image:
-        '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-    }];
-  res.json(data);
-});
+
 
 const objToExport = {
   getMoviesFromApi: getMoviesFromApi
