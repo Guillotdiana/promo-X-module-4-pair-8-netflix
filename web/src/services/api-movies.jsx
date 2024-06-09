1ba8d31120da88e7379c044ce14c83aee4a871af
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-06-09 20:36:23 +0200

    adds genre query param to getMoviesFromApi

--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,9 +1,21 @@
 // login
 
-const getMoviesFromApi = () => {
+// Se agregan los parametros que llegan desde App.jsx
+const getMoviesFromApi = ({genre, sort}) => {
   console.log('Se están pidiendo las películas de la app');
-  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//localhost:4000/movies')
+  // Se muestran en consola para ver su contenido
+  console.log(`Genre: ${genre}`);
+  console.log(`Sort: ${sort}`);
+
+  // Se define la url de la api
+  let apiUrl = '//localhost:4000/movies';
+
+  // Si esta definido el genre, se agrega como query param a la peticion
+  if (genre) {
+    apiUrl = `${apiUrl}?genre=${genre}`;
+  }
+
+  return fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
