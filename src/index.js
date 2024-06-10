71184fcb9fd25e5286049801d6e8ee382c943635
rafaelapessoa <rafaela_tunu@hotmail.com>
2024-06-10 11:28:48 +0200

    post metod

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
+const jwt = require('jsonwebtoken');
 
 // create and config server
 const server = express();
@@ -23,6 +24,23 @@ server.get('/movies', async (req, res) => {
   let selectMovies = 'SELECT * FROM movies';
   const queryParams = [];
 
+  api.post('/api/register', async (req, res) => {
+    const conex = away conexion();
+    const { email, pass } = req.body;
+    const selectEmail = 'SELECT *FROM user WHERE email = ?';
+    const [emailResult] = await conex.query(selectEmail, [email]);
+
+    if (emailResult.length === 0) {
+      const hasshedPassword = await bcrypt.hash(pass, 10);
+      const insertUser =
+        'INSERT INTO user (email, hashed_password) values (?,?)';
+      const [newUser] = await conex.query(insertUser, [email, hasshedPassword]);
+      res.status(201).json({ success: true, id: newUser.insertId });
+    } else {
+      res.status(200).json({ success: false, message: 'usuario ya existe' });
+    }
+  });
+
   // Si genreFilterParams esta definido, modifica la query y agrega un parametrp
   if (genreFilterParams) {
     selectMovies += ' WHERE genre = ?';
@@ -43,9 +61,6 @@ server.get('/movies', async (req, res) => {
   res.json({ success: true, movies: results });
 });
 
-api.use(express.static)
-
-
 async function conexion() {
   //defino la ubicacion y datos de BD
   const conn = await mysql.createConnection({
@@ -58,7 +73,6 @@ async function conexion() {
   await conn.connect();
   return conn;
 }
-
 
 //Servidor de estaticos. Debe estar al final para que primero se renderice la peticion
 server.use(express.static("src/public-movies-images"))
--- web/src/services/api-user.jsx
+++ web/src/services/api-user.jsx
@@ -29,15 +29,13 @@ const sendSignUpToApi = (data) => {
   console.log("Se están enviando datos al signup:", data);
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   return fetch(
-    "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json"
+    "//localhost:4000/sign-up"
   )
     .then((response) => response.json())
-    .then(() => {
+    .then (data => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: false,
-        errorMessage: "Usuario ya existente",
-      };
+  return data;
+        
     });
 };
 
