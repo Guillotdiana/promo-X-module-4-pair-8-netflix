ee98db717c280c81583e64ecaa97b2621424cc09
Diana <dianaga93@hotmail.com>
2024-06-11 11:28:46 +0200

    add

--- src/index.js
+++ src/index.js
@@ -13,6 +13,20 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+async function conexion() {
+  //defino la ubicacion y datos de BD
+  const conn = await mysql.createConnection({
+    host: 'sql.freedb.tech',
+    user: 'freedb_netflix_admin',
+    password: 'avtZ%j!F*Y$u92U',
+    database: 'freedb_netflix',
+  });
+  //me conecto a la BD  definida
+  await conn.connect();
+  return conn;
+}
+
 server.get('/movies', async (req, res) => {
   const conn = await conexion();
   const genreFilterParams = req.query.genre;
@@ -24,22 +38,7 @@ server.get('/movies', async (req, res) => {
   let selectMovies = 'SELECT * FROM movies';
   const queryParams = [];
 
-  api.post('/api/register', async (req, res) => {
-    const conex = await conexion();
-    const { email, pass } = req.body;
-    const selectEmail = 'SELECT *FROM user WHERE email = ?';
-    const [emailResult] = await conex.query(selectEmail, [email]);
 
-    if (emailResult.length === 0) {
-      const hasshedPassword = await bcrypt.hash(pass, 10);
-      const insertUser =
-        'INSERT INTO user (email, hashed_password) values (?,?)';
-      const [newUser] = await conex.query(insertUser, [email, hasshedPassword]);
-      res.status(201).json({ success: true, id: newUser.insertId });
-    } else {
-      res.status(200).json({ success: false, message: 'usuario ya existe' });
-    }
-  });
 
   // Si genreFilterParams esta definido, modifica la query y agrega un parametrp
   if (genreFilterParams) {
@@ -61,18 +60,24 @@ server.get('/movies', async (req, res) => {
   res.json({ success: true, movies: results });
 });
 
-async function conexion() {
-  //defino la ubicacion y datos de BD
-  const conn = await mysql.createConnection({
-    host: 'sql.freedb.tech',
-    user: 'freedb_netflix_admin',
-    password: 'avtZ%j!F*Y$u92U',
-    database: 'freedb_netflix',
-  });
-  //me conecto a la BD  definida
-  await conn.connect();
-  return conn;
-}
+
+
+server.post('/register', async (req, res) => {
+  const conex = await conexion();
+  const { email, password } = req.body;
+  const selectEmail = 'SELECT * FROM user WHERE email = ?';
+  const [emailResult] = await conex.query(selectEmail, [email]);
+
+  if (emailResult.length === 0) {
+    const hasshedPassword = await bcrypt.hash(password, 10);
+    const insertUser =
+      'INSERT INTO user (email, hashed_password) values (?,?)';
+    const [newUser] = await conex.query(insertUser, [email, hasshedPassword]);
+    res.status(201).json({ success: true, id: newUser.insertId });
+  } else {
+    res.status(200).json({ success: false, message: 'usuario ya existe' });
+  }
+});
 
 //Servidor de estaticos. Debe estar al final para que primero se renderice la peticion
 server.use(express.static("src/public-movies-images"))
--- web/src/services/api-user.jsx
+++ web/src/services/api-user.jsx
@@ -4,23 +4,27 @@ const sendLoginToApi = (data) => {
   console.log("Se están enviando datos al login:", data);
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   return fetch(
-    "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json"
+     "//localhost:4000/register"
   )
-    .then((response) => response.json())
-    .then(() => {
+  .then(response => response.json())
+  .then(data => {
+   return data;
+  });
+    //.then((response) => response.json())
+    //.then((data) => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      if (data.email.includes("gmail")) {
-        return {
-          success: true,
-          userId: "123",
-        };
-      } else {
-        return {
-          success: false,
-          errorMessage: "Usuario no encontrado",
-        };
-      }
-    });
+      // if (data.email.includes("gmail")) {
+      //   return {
+      //     success: true,
+      //     userId: "123",
+      //   };
+      // } else {
+      //   return {
+      //     success: false,
+      //     errorMessage: "Usuario no encontrado",
+      //   };
+      // }
+   // });
 };
 
 // signup
